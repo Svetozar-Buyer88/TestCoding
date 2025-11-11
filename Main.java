f0a6f348d9d64c49bda6f6a764970d209af5a2be
Pavel <stepkin.pvl@gmail.com>
2025-11-11 21:31:13 +0300

    3 коммит

--- Main.java
+++ Main.java
@@ -2,25 +2,38 @@ import java.util.*;
 
 class Main {
 
-    public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
 
-        // game loop
-        while (true) {
-            int x = in.nextInt();
-            int y = in.nextInt();
-            int nextCheckpointX = in.nextInt(); // x position of the next check point
-            int nextCheckpointY = in.nextInt(); // y position of the next check point
-            int nextCheckpointDist = in.nextInt(); // distance to the next checkpoint
-            int nextCheckpointAngle = in.nextInt(); // угол между ориентацией вашего модуля и направлением на следующую контрольную точку
-            int opponentX = in.nextInt();
-            int opponentY = in.nextInt();
+        public static void main(String args[]) {
+            Scanner in = new Scanner(System.in);
+            boolean boostUsed=false;
+            // game loop
+            while (true) {
+                int x = in.nextInt();
+                int y = in.nextInt();
+                int nextCheckpointX = in.nextInt(); // x position of the next check point
+                int nextCheckpointY = in.nextInt(); // y position of the next check point
+                int nextCheckpointDist = in.nextInt(); // distance to the next checkpoint
+                int nextCheckpointAngle = in.nextInt(); // angle between your pod orientation and the direction of the next checkpoint
+                int opponentX = in.nextInt();
+                int opponentY = in.nextInt();
+                int thrust = 0;
+                boolean boost=false; // флаг буста(изначально false не используется)
 
-            if(nextCheckpointAngle > 90 || nextCheckpointAngle < -90){
-                System.out.println(nextCheckpointX + " " + nextCheckpointY + " 0");
+
+                if(nextCheckpointAngle > 90 || nextCheckpointAngle < -90){
+                    thrust=0;
+                }
+
+                else {
+                    thrust=100;
+                    if(nextCheckpointDist > 6000 && !boostUsed){
+                        boost = true;
+                        boostUsed = true;
+                    }
+                }
+                System.out.println(nextCheckpointX +" "+nextCheckpointY +" "+(boost ? "BOOST" : thrust)
+                );
+
             }
-            else
-                System.out.println(nextCheckpointX + " " + nextCheckpointY + " 100");
         }
     }
-}
