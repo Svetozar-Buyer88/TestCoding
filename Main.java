f1d08ea3ca160fd8b7776b754e9bfe5130e2ff80
Pavel <stepkin.pvl@gmail.com>
2025-11-11 20:44:57 +0300

    2 коммит

--- Main.java
+++ Main.java
@@ -7,14 +7,20 @@ class Main {
 
         // game loop
         while (true) {
-            int x = in.nextInt(); // x position of your pod
-            int y = in.nextInt(); // y position of your pod
+            int x = in.nextInt();
+            int y = in.nextInt();
             int nextCheckpointX = in.nextInt(); // x position of the next check point
             int nextCheckpointY = in.nextInt(); // y position of the next check point
+            int nextCheckpointDist = in.nextInt(); // distance to the next checkpoint
+            int nextCheckpointAngle = in.nextInt(); // угол между ориентацией вашего модуля и направлением на следующую контрольную точку
+            int opponentX = in.nextInt();
+            int opponentY = in.nextInt();
 
-            System.out.println(nextCheckpointX + " " + nextCheckpointY + " 100");
-
-
+            if(nextCheckpointAngle > 90 || nextCheckpointAngle < -90){
+                System.out.println(nextCheckpointX + " " + nextCheckpointY + " 0");
+            }
+            else
+                System.out.println(nextCheckpointX + " " + nextCheckpointY + " 100");
         }
     }
 }
